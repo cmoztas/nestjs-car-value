7c0e4ba0279e0cbba9094ddfa1a9a6ab4464cb51
cagkanoztas <cagkan.oztas@jengal.com>
2023-09-17 23:39:36 +0300

    finding and filtering records

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UsersService } from "./users.service";
+import { User } from "./user.entity";
 
 @Controller("auth")
 export class UsersController {
@@ -12,5 +13,15 @@ export class UsersController {
   @Post("/signup")
   createUser(@Body() body: CreateUserDto): void {
     void this.usersService.create(body);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string): Promise<User> {
+    return this.usersService.findOne(+id);
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string): Promise<User[]> {
+    return this.usersService.find(email);
   }
 }
