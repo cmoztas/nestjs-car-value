9ae9ab87e1c6e7776e442a3bcb1e500ca1a4533a
cagkanoztas <cagkan.oztas@jengal.com>
2023-09-17 23:03:11 +0300

    create and save user

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrap(): Promise<void> {
+  const app: INestApplication = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true
+    })
+  );
+
   await app.listen(3000);
 }
-bootstrap();
+
+void bootstrap();
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { User } from "./user.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { CreateUserDto } from "./dtos/create-user.dto";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private repo: Repository<User>
+  ) {
+  }
+
+  create(user: CreateUserDto): Promise<User> {
+    const createdUser: User = this.repo.create({
+      email: user.email,
+      password: user.password
+    });
+
+    return this.repo.save(createdUser);
+  }
+}
